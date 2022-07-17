13cbccc2cc7b5779afb3f3205681e9f4b26a347c
zb-gong <gongzib@outlook.com>
2022-07-17 02:37:59 +0000

    added docker stat script

---
+++ tools/docker_stat_monitor.sh
@@ -0,0 +1,5 @@
+for (( ; ;)) do 
+  sudo docker stats --no-stream 96edd256c25b | tail -1 | awk '{print $3}' >> docker.txt
+  echo ----------------- 
+  sleep 2
+done
--- tools/util_monitor.sh
+++ tools/util_monitor.sh
@@ -1,5 +1,7 @@
 for (( ; ;)) do 
-  pgrep python3 | xargs -I {} ps -p {} -L -o psr,pcpu
+  # echo 815086 | xargs -I {} ps -p {} -L -o psr,pcpu > util.txt
+  # echo 707254 | xargs -I {} ps -p {}  -o pcpu
+  top -b -n 2 -d 0.2 -p 815086 | tail -1 | awk '{print $9}' >> util.txt
   echo ----------------- 
-  sleep 1
+  sleep 2
 done
